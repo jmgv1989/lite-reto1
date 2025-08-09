e6fb300e5971a48fab0da5335282be95e3502f5d
jmgv1989 <jmgv1989@gmail.com>
2025-08-09 15:00:59 -0400

    se corrige pom para error de arraqnue, se agrega logger, se especifica columnas en la entidad

--- src/main/java/com/josegal/producto1rp/controller/ProductController.java
+++ src/main/java/com/josegal/producto1rp/controller/ProductController.java
@@ -6,33 +6,39 @@ import org.springframework.web.bind.annotation.*;
 import com.josegal.producto1rp.domain.model.Product;
 import com.josegal.producto1rp.exception.ProductNotFoundException;
 import com.josegal.producto1rp.repository.ProductRepository;
-
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping
+    @GetMapping("/findAll")
     public List<Product> getAllProducts() {
+        logger.info("Mensaje de getAllProducts ");
         return productRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable Long id) {
+        logger.info("Mensaje de getProductById: "+id);
         return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public Product createProduct(@RequestBody Product product) {
+        logger.info("Mensaje de createProduct: "+product);
         return productRepository.save(product);
     }
 
     @PutMapping("/{id}")
     public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        logger.info("Mensaje de updateProduct: "+ productDetails);
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
 
@@ -45,8 +51,15 @@ public class ProductController {
 
     @DeleteMapping("/{id}")
     public String deleteProduct(@PathVariable Long id) {
+        logger.info("Mensaje de deleteProduct");
         Product product = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
         productRepository.delete(product);
         return "Product " + id + " deleted successfully!";
+    }
+
+    @GetMapping("/test/{id}")
+    public String testIdProduct(@PathVariable Long id) {
+        logger.info("Mensaje de testIdProduct");
+        return "Product " + id + " recivido";
     }
 }
--- src/main/java/com/josegal/producto1rp/domain/model/Product.java
+++ src/main/java/com/josegal/producto1rp/domain/model/Product.java
@@ -1,19 +1,25 @@
 package com.josegal.producto1rp.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String name;
+    @Column
     private Double price;
+    @Column
     private Integer quantity;
 
     // Getters and Setters
